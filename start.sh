9840075fb113f9904a4a9be334a813c64ea7e420
vitorluiz <vitorluizmachado@gmail.com>
2023-10-06 22:06:51 -0400

    Adicionando saida de log nos comandos

--- start.sh
+++ start.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
 # Input info
-read -p "Entre com o IP do Servidor: " IP
+read -p "Entre com o IP do Servidor: " IP &>> $LOG
+
+# ARQUIVO DE LOG
+LOGSCRIPT="/var/log/$(echo $0 | cut -d'/' -f2)"
+
+# Arquivo de configuração dos parâmetros utilizados nesse script
+# source 00-parametros.sh
+#
+# Configuração da variável de Log utilizado nesse script
+LOG=$LOGSCRIPT
 
 # ATUALIZANDO E INSTALANDO OS PACOTES ESSENCIAIS
-apt update && \ 
-apt install build-essential git wget unzip curl apparmor apparmor-utils && \ 
-apt install upgrade -y
+apt update &>> $LOG && \ 
+apt install build-essential git wget unzip curl apparmor apparmor-utils &>> $LOG && \ 
+apt install upgrade -y &>> $LOG
 
 # ACESSANDO O DIRETORIO ROOT
-cd ~
+cd ~ &>> $LOG
 
 # INSTALANDO O DOCKER CE
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+curl -fsSL https://get.docker.com -o get-docker.sh &>> $LOG
+sh get-docker.sh &>> $LOG
 
 # HABILITANDO O MODO SWARM NO DOCKER CE 
-docker swarm init --advertise-addr $IP
+docker swarm init --advertise-addr $IP &>> $LOG
 
 # PORTAINER
-mkdir portainer && cd portainer
-curl -L https://downloads.portainer.io/ce2-19/portainer-agent-stack.yml -o portainer-agent-stack.yml
-docker stack deploy -c portainer-agent-stack.yml portainer
+mkdir portainer && cd portainer &>> $LOG
+curl -L https://downloads.portainer.io/ce2-19/portainer-agent-stack.yml -o portainer-agent-stack.yml &>> $LOG
+docker stack deploy -c portainer-agent-stack.yml portainer &>> $LOG
 
 # ACESSANDO O DIRETORIO ROOT
-cd ~
+cd ~ &>> $LOG
